00f4afa8ed2851e888e264522a8ba1808e45a09c
Rodrigo Tavares <rodrigo.tavares.lima@hotmail.com>
2022-08-09 07:50:43 -0300

    fix: landing removed from app.component.html

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UserSignInComponent } from "./user-sign-in/user-sign-in.component";
+import { UserSignUpComponent } from "./user-sign-up/user-sign-up.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+
+const routes: Routes = [
+  { path: "", component: LandingPageComponent },
+  { path: "registrar", component: UserSignUpComponent },
+  { path: "entrar", component: UserSignInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserSignInComponent } from './user-sign-in/user-sign-in.component';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
+import { FormInputComponent } from './form-input/form-input.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserSignInComponent,
-    UserSignUpComponent
+    UserSignUpComponent,
+    FormInputComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
